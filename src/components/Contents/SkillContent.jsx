d2722608097551f51c19def86760cdd4218ad3f7
min <a0955361101@gmail.com>
2022-08-28 01:58:17 +0800

    skill page

--- src/components/Contents/SkillContent.jsx
+++ src/components/Contents/SkillContent.jsx
@@ -7,17 +7,51 @@ import Jquery from '../Icons/Jquery';
 import NodeJs from '../Icons/NodeJs';
 import GitHub from '../Icons/GitHub';
 function SkillContent() {
-
+    const skills = [
+        {
+            name: 'JavaScript',
+            svg: <JavaScript />
+        }, {
+            name: 'HTML',
+            svg: <Html />
+        },
+        {
+            name: 'CSS',
+            svg: <Css />
+        },
+        {
+            name: 'Bootstrap',
+            svg: <Bootstrap />
+        },
+        {
+            name: 'React',
+            svg: <ReactIcon />
+        },
+        {
+            name: 'jQuery',
+            svg: <Jquery />
+        },
+        {
+            name: 'Node.js',
+            svg: <NodeJs />
+        },
+        {
+            name: 'GitHub',
+            svg: <GitHub />
+        },
+    ];
     return (
-        <div className="ContentWrap">
-            <JavaScript />
-            <Html />
-            <Css />
-            <Bootstrap />
-            <ReactIcon />
-            <Jquery />
-            <NodeJs />
-            <GitHub />
+        <div className="SkillContent">
+            <div className='skill-icon-wrap'>
+                {skills.map((v, i) => {
+                    return (
+                        <div className='skill-icon' key={i}>
+                            {v.svg}
+                            <p>{v.name}</p>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 
