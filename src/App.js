ae30807dab83efa2c4e7b7a4b5e757ee90a9ce8f
min <a0955361101@gmail.com>
2022-08-28 12:36:15 +0800

    nav

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import ContentWrap from "./components/Contents/ContentWrap";
 import SkillContent from "./components/Contents/SkillContent";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
-  const [getTag, setGetTag] = useState('');
+  const [getTag, setGetTag] = useState('全部');
   const [getPath, setGetPath] = useState('作品');
 
   const handlePath = (v) => {
     setGetPath(v);
   };
+  useEffect(() => {
+    if (getTag === undefined) {
+      setGetTag('全部');
+    }
+  }, [getTag]);
   return (<div style={{
     width: '100%', height: '300vh'
   }}>
