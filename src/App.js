a1a212054ad7809c9a3919e3feb930498223c72b
min <a0955361101@gmail.com>
2022-08-31 20:24:52 +0800

    NAV

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
-import ContentWrap from "./components/Contents/ContentWrap";
-import SkillContent from "./components/Contents/SkillContent";
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import ContentWrap from './components/Contents/ContentWrap';
+import SkillContent from './components/Contents/SkillContent';
 import { useState } from 'react';
 
-
 function App() {
-  const [getTag, setGetTag] = useState('全部');
-  const [getPath, setGetPath] = useState('作品');
+    const [getTag, setGetTag] = useState('全部');
+    const [getPath, setGetPath] = useState('作品');
 
-  const handlePath = (v) => {
-    setGetPath(v);
-  };
+    const handlePath = (v) => {
+        setGetPath(v);
+    };
 
-  return (<div style={{
-    width: '100%', height: '300vh'
-  }}>
-    <Header setGetTag={setGetTag} getPath={getPath} />
-    <Navbar getPath={getPath} handlePath={handlePath} />
-    {getPath === '作品' && getTag === '全部' ? <ContentWrap getTag={getTag} /> : ''}
-    {getPath === '技能' ? <SkillContent /> : ''}
 
-  </div>
+    return (
+        <div
+            style={{
+                width: '100%',
+                height: '300vh',
+            }}>
+            <Header setGetTag={setGetTag} getPath={getPath} />
+            <Navbar getPath={getPath} handlePath={handlePath} />
+            {getPath === '作品' ? (
+                <ContentWrap getTag={getTag} />
+            ) : (
+                ''
+            )}
+            {getPath === '技能' ? <SkillContent /> : ''}
 
-  );
+        </div>
+    );
 }
 
 export default App;
