eb3375f806e98cfe5939e94a1661debbfff0723e
min <a0955361101@gmail.com>
2022-09-06 19:15:21 +0800

    新增作品

--- src/components/Contents/Card.jsx
+++ src/components/Contents/Card.jsx
@@ -3,7 +3,7 @@ function Card({ data }) {
     return (
         <div className="Card">
             <a href={data.url} target="_blank" rel="noreferrer"><img src={`${data.img}`} alt="work" /></a>
-            <div className="Card-txt">
+            <div className="Card-txt pt-3">
                 <h3>{data.name}</h3>
                 <p>{data.people}作品</p>
                 <p>使用技術:{data.skill}</p>
